0c2c976362785b37178908921b22cbae6d14a668
Artyom Yeprikyan <artyomyeprikyan@outlook.com>
2023-05-23 02:39:42 +0400

    Added Product adding functionality for check

---
+++ Apex.GameZone.API/MapperProfiles/ProductCheckoutProfile.cs
@@ -0,0 +1,15 @@
+﻿using Apex.GameZone.API.ViewModels.Checkout;
+using Apex.GameZone.Core.Models;
+using AutoMapper;
+
+namespace Apex.GameZone.API.MapperProfiles
+{
+    public class ProductCheckoutProfile : Profile
+    {
+        public ProductCheckoutProfile()
+        {
+            CreateMap<ProductCheckoutViewModel, ProductCheckoutModel>();
+            CreateMap<ProductCheckoutModel, ProductCheckoutViewModel>();
+        }
+    }
+}
---
+++ Apex.GameZone.Core/MapperProfiles/ProductCheckoutProfile.cs
@@ -0,0 +1,14 @@
+﻿using Apex.GameZone.Core.Models;
+using Apex.GameZone.Data.Entities;
+using AutoMapper;
+
+namespace Apex.GameZone.Core.MapperProfiles;
+
+public class ProductCheckoutProfile : Profile
+{
+    public ProductCheckoutProfile()
+    {
+        CreateMap<ProductCheckoutModel, ProductCheckoutEntity>();
+        CreateMap<ProductCheckoutEntity, ProductCheckoutModel>();
+    }
+}
